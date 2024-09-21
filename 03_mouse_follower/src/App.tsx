99639d5e1e28dbf9e41367cc7f5962ab1931627e
David Marti <davidmartisangu@DavidPC>
2024-09-21 17:58:06 +0200

    adding proyect 03

---
+++ 03_mouse_follower/src/App.tsx
@@ -0,0 +1,12 @@
+import {FollowMouse} from "./components/followMouse"
+
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
---
+++ 03_mouse_follower/src/components/followMouse.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+
+export const FollowMouse = ()=>{
+    const[enabled, setEnabled] = useState(false)
+    const[position, setPosition] = useState({x:0, y:0})
+  
+    useEffect(()=>{
+      console.log("estado: ", {enabled})
+  
+      const handleMove = (event:MouseEvent)=>{
+        const {clientX, clientY} = event
+        setPosition({x:clientX, y: clientY})
+      }
+  
+      if (enabled){
+        window.addEventListener("pointermove", handleMove)
+      }
+  
+      return ()=>{
+        window.removeEventListener("pointermove", handleMove)
+      }
+    }, [enabled])
+  
+    return(
+      <>
+        <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}>
+        </div>
+        <button onClick={()=>setEnabled(!enabled)}>
+          {enabled ? 'desactivar' : 'activar' } seguimiento del puntero
+        </button>
+      </>
+    )
+  } 
